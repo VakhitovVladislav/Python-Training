c9183f3ea6dfc36bb0369d0e09dfb09967022662
VakhitovVladislav <67861615+VakhitovVladislav@users.noreply.github.com>
2021-05-15 23:03:44 +0800

    10 задание. оптимизированы переходы между страницами

--- fixture/Contact_helper.py
+++ fixture/Contact_helper.py
@@ -8,7 +8,8 @@ class ContactHelper:
 
     def return_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_xpath("//input[@value='Send e-Mail']")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def create(self, contact):
         wd = self.app.wd
