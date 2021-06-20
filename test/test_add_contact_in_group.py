8a2423f21d684fe086e85148f93745f9d7411af3
VakhitovVladislav <67861615+VakhitovVladislav@users.noreply.github.com>
2021-06-20 14:50:35 +0800

    v.22.0.7

--- test/test_add_contact_in_group.py
+++ test/test_add_contact_in_group.py
@@ -8,15 +8,15 @@ def test_add_contact_in_group(app, db):
         app.contact.create(Contact(firstname="Vlad", lastname="hater"))
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
-    if len(db.get_contact_not_in_group()) == 0:
+    if len(db.get_contacts_not_in_group()) == 0:
         app.contact.create(Contact(firstname="Vlad", lastname="hater"))
-    old_groups = app.group.get_group_list()
-    random_group = random.choice(old_groups)
-    group_db_id = random_group.id
-    contacts_not_in_group = db.get_contact_not_in_group()
-    random_contact = random.choice(contacts_not_in_group)
-    random_contact_id = random_contact[0]
-    old_contact_in_group = db.get_contact_in_group(group_db_id)
-    app.contact.add_contact_to_group(random_contact_id, group_db_id)
-    new_contacts_in_group = db.get_contact_in_group(group_db_id)
+    group = app.group.get_group_list()
+    groups = random.choice(group)
+    group_id = groups.id
+    contacts = app.contact.get_contact_list()
+    contact = random.choice(contacts)
+    contact_id = contact.id
+    old_contact_in_group = db.get_contacts_in_group()
+    app.contact.add_contact_to_group(contact_id, group_id)
+    new_contacts_in_group = db.get_contacts_in_group()
     assert len(old_contact_in_group) + 1 == len(new_contacts_in_group)
