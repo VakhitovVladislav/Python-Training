ae0b42d78980305aedb833a2703b9d627837d38d
VladislavVakhitov <siemines11@gmail.com>
2021-06-29 14:13:29 +0800

    screen

--- generator/contact.py
+++ generator/contact.py
@@ -35,7 +35,7 @@ def random_email(maxlen):
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    symbols = string.ascii_letters + string.digits + string.punctuation + ""*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
